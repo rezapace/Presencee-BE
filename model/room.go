124c3023d22e3c2d314ca8883866e0b68189df21
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-06-04 14:46:35 +0700

    Add Room model

---
+++ model/room.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type Room struct {
+	gorm.Model
+	Name     string `json:"name" form:"name"`
+	Location string `json:"location" form:"location"`
+	Seat     string `json:"seat" form:"seat"`
+}
